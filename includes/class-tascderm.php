a22d08018c35b135134b36ed56c6add2676bcaa3
dermatologist <github@gulfdoctor.net>
2016-06-03 18:19:42 -0400

    tascderm working

--- includes/class-tascderm.php
+++ includes/class-tascderm.php
@@ -42,17 +42,61 @@ class WPD_Tascderm
      */
     public function hooks()
     {
+        add_shortcode( 'tascderm', array( $this, 'render_tascderm' ));
+        add_action('admin_post_nopriv_submit-form', array( $this, '_handle_form_action'));
+        add_action('admin_post_submit-form', array( $this, '_handle_form_action'));
     }
 
-    public function render_tascderm($content)
+    function _handle_form_action(){
+
+
+        if(isset($_POST['tascderm'])){
+            $tascderm_entered = $_POST['tascderm'];
+            $tascderm = get_post_meta( $_POST['postId'],
+                '_wp_dermatology_tascderm', true );
+            if($tascderm_entered){
+                $tascderm = ($tascderm_entered + $tascderm)/2;
+            }
+
+            update_post_meta( $_POST['postId'],
+                '_wp_dermatology_tascderm',
+                $tascderm
+            );
+        }
+
+
+
+        wp_redirect( $_SERVER['HTTP_REFERER'] );
+        exit();
+
+
+    }
+
+    public function render_tascderm()
     {
         global $post;
-        $value = get_post_meta( $post->ID,
-            '_wp_dermatology_tascderm', true ); //array
+        $tascderm = get_post_meta( $post->ID,
+            '_wp_dermatology_tascderm', true );
 
-        $content .= "
+
+        $content = "
+<script>
+function wp_dermatology_findTotal(){
+    var arr = document.getElementsByName('wp_dermatology_tascderm');
+    var tot=0;
+    for(var i=0;i<arr.length;i++){
+        if(parseInt(arr[i].value))
+            tot += parseInt(arr[i].value);
+    }
+    document.getElementById('tascderm_total').value = tot;
+}
+</script>
 <div id='tascderm'>
-    <form>
+    <h2>TASCDerm Scoring Table</h2>
+    <h3>Mean TASDerm Score:".$tascderm."</h3>
+    <form action='".get_admin_url()."admin-post.php' method='post'>
+        <input type='hidden' name='action' value='submit-form' />
+        <input type='hidden' name='postId' value='".$post->ID."' />
         <table class='tascderm-table'>
             <thead>
                 <tr>
@@ -64,193 +108,143 @@ class WPD_Tascderm
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if no randomization performed or randomization not mentioned.' data-animation='true' data-placement='top' data-html='false'>Randomization</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[0]' id='wp_dermatology_tascderm[0]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='2'
-                                <?php if ( '2' == $value ) echo 'selected';
-                            ?>>2</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[0]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if no placebo, 1 if inert placebo, 2 if compared to gold standard.' data-animation='true' data-placement='top' data-html='false'>Placebo</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[1]' id='wp_dermatology_tascderm[1]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='2'
-                                <?php if ( '2' == $value ) echo 'selected';
-                            ?>>2</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[1]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 No blinding, 1 Single blinded, 2 Double blinded' data-animation='true' data-placement='top' data-html='false'>Blinding</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[2]' id='wp_dermatology_tascderm[2]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='2'
-                                <?php if ( '2' == $value ) echo 'selected';
-                            ?>>2</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[2]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='Example: 1 if Colorimeter is used for melasma and 0 if MASI is used.' data-animation='true' data-placement='top' data-html='false'>Objective measurement</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[3]' id='wp_dermatology_tascderm[3]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[3]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='Effect size of 0.5, alpha 0.05 and power 80' data-animation='true' data-placement='top' data-html='false'>Sample size more than 21</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[4]' id='wp_dermatology_tascderm[4]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[4]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if study is done on clinic staff or if person knows allocated group. 1 if appropriate.' data-animation='true' data-placement='top' data-html='false'>Sample selection and allocation concealment</span></td>
-                    <td class='score'>
-                        <select name='wp_dermatology_tascderm[5]' id='wp_dermatology_tascderm[5]'
+                    <td class='score' onchange='wp_dermatology_findTotal();'>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[5]'
                             class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='1 if intervention cheaper than gold standard' data-animation='true' data-placement='top' data-html='false'>Economical</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[6]' id='wp_dermatology_tascderm[6]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[6]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if present, 1 if absent' data-animation='true' data-placement='top' data-html='false'>Conflict of interest</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[7]' id='wp_dermatology_tascderm[7]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[7]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if more than 1 in 5 (20%) or not mentioned, 1 if not' data-animation='true' data-placement='top' data-html='false'>Withdrawal / Dropouts</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[8]' id='wp_dermatology_tascderm[8]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[8]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if not mentioned or inappropriate, 1 if appropriate' data-animation='true' data-placement='top' data-html='false'>Coherent endpoints </span></td>
-                    <td class='score'>            	            <select name='wp_dermatology_tascderm[9]' id='wp_dermatology_tascderm[9]'
-                        class='postbox'>
-                        <option value='0'
-                            <?php if ( '0' == $value ) echo 'selected';
-                        ?>>1</option>
-                        <option value='1'
-                            <?php if ( '1' == $value ) echo 'selected';
-                        ?>>1</option>
+                    <td class='score'>
+                    <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[9]'
+                        class='postbox' onchange='wp_dermatology_findTotal();'>
+                        <option value='0' selected>0</option>
+                        <option value='1'>1</option>
                     </select></td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if multiple active ingredients tested, 1 if single' data-animation='true' data-placement='top' data-html='false'>Co-Interventions</span></td>
                     <td class='score'>
-                        <select name='wp_dermatology_tascderm[10]' id='wp_dermatology_tascderm[10]'
-                            class='postbox'>
-                            <option value='0'
-                                <?php if ( '0' == $value ) echo 'selected';
-                            ?>>1</option>
-                            <option value='1'
-                                <?php if ( '1' == $value ) echo 'selected';
-                            ?>>1</option>
+                        <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[10]'
+                            class='postbox' onchange='wp_dermatology_findTotal();'>
+                            <option value='0' selected>0</option>
+                            <option value='1'>1</option>
                         </select>
                     </td>
                 </tr>
                 <tr>
                     <td class='criteria'><span class=' bs-tooltip' title='0 if no rational theoretical basis, 1 if sound' data-animation='true' data-placement='top' data-html='false'>Acceptable theory</span></td>
-                    <td class='score'>            	            <select name='wp_dermatology_tascderm[11]' id='wp_dermatology_tascderm[11]'
-                        class='postbox'>
-                        <option value='0'
-                            <?php if ( '0' == $value ) echo 'selected';
-                        ?>>1</option>
-                        <option value='1'
-                            <?php if ( '1' == $value ) echo 'selected';
-                        ?>>1</option>
+                    <td class='score'>
+                    <select name='wp_dermatology_tascderm' id='wp_dermatology_tascderm[11]'
+                        class='postbox' onchange='wp_dermatology_findTotal();'>
+                        <option value='0' selected>0</option>
+                        <option value='1'>1</option>
                     </select></td>
                 </tr>
                 <tr>
                     <td class='total'>Total</td>
-                    <td class='total'><span class='digit'></span>/15</td>
+                    <td class='total'><input type='text' name='tascderm' id='tascderm_total' value = '0' /></td>
                 </tr>
             </tbody>
         </table>
+        <input type='submit' name='save_data' value='Score'/>
     </form>
 </div>
         ";
+        return $content;
     }
 
-    public function render_tascderm_interpretation($content)
+    public function render_tascderm_interpretation()
     {
 
         if (is_single()) {
 
-            $content .= "
+            $content = "
 <div id='tascderm-interpretation'>
     <table class='tascderm-table'>
         <caption><i class='fa fa-star'></i>Interpretation is as follows:</caption>
